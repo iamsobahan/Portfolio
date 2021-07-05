184143519ad9af96aee9d39c939620ab4d4e0a90
unknown <you@example.com>
2021-07-05 21:42:41 +0600

    adding particles and typed js

---
+++ script/script.js
@@ -0,0 +1,32 @@
+
+
+
+
+var type = new Typed('#typed-string', {
+    stringsElement: '#typed',
+    typeSpeed: 200,
+    loop: true,
+    loopCount: 10,
+    backspeed: 1000,
+
+});
+
+
+
+$('.canvas').particles({
+
+    maxParticles: 1000,
+
+    size: 3,
+
+    speed: 0.5,
+
+    color: '#000000',
+
+    minDist: 140,
+
+    connectParticles: false
+
+});
+
+
